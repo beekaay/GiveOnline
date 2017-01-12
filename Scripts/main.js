e22e98cddee0b8d95559d2f3b8a78eda78bb62d5
beekaay <andersak88@gmail.com>
2017-01-12 21:52:46 +0100

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,34 @@
+﻿
+
+$(document).ready(function () {
+    if ('@ViewBag.Message' == 'Sent') {
+        alert('Mail has been sent successfully');
+    }
+
+    //$('#summernote').summernote({
+    //    toolbar: [
+    //      // [groupName, [list of button]]
+    //      ['style', ['bold', 'italic', 'underline']],
+    //      ['font', ['strikethrough', 'superscript', 'subscript']],
+    //      ['fontsize', ['fontsize']],
+    //      ['color', ['color']],
+    //      ['para', ['ul', 'ol', 'paragraph']],
+    //      ['insert', ['hr']],
+    //      ['misc', ['codeview', 'undo', 'redo']]
+    //    ]
+    //});
+
+});
+
+$(function () {
+    $('#datetimepicker1').datetimepicker({
+        format: 'DD/MM/YYYY hh:mm',
+    });
+});
+
+$(function () {
+    $('#datetimepicker2').datetimepicker({
+        format: 'DD/MM/YYYY hh:mm',
+    });
+});
+
